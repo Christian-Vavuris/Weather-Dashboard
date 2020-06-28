13171c5793c97a1637b3fc40ebbbf5dc59097b8a
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-06-28 10:53:29 -0700

    added support for small screens

--- weather.js
+++ weather.js
@@ -1,16 +1,9 @@
 var cityInputEl = document.getElementById("city-input");
 var cityButton = document.getElementById("button-addon2");
-var searchedCities = [
-    {
-        name:"San Francisco"
-    },
-    {
-        name:"Detroit"
-    },
-    {
-        name:"Baton Rouge"
-    }
-]
+var cityList = document.getElementById("city-list")
+var placeholderCity = document.getElementById("placeholder-city")
+var searchedCities = []
+
 
 
 
@@ -21,18 +14,38 @@ var getCityName = function() {
     }
     else {
     console.log(cityName);
+    saveCityName(cityName);
+    displaySearchedCities();
+    cityInputEl.value = "";
+
     }
     return cityName, getCurrentWeatherData();
 }
 
-//Save the selected name to local storage and append it to the ul (Office Hours)
+//Save the selected name to the Array, add it local storage. Have names persist. I can't get it to save to local storage when I dont have an array already in there
 
-// var saveCityName = function () {
-//     localStorage.setItem('cities', stringCities)
+var saveCityName = function (city) {
 
-// }
+    // pull array from Local Storage first
+    var retrievedCities = localStorage.getItem('searchedCities');
+    var citiesList = JSON.parse(retrievedCities); 
+    citiesList.push(city);
+    // console.log(searchedCities)
+    localStorage.setItem("searchedCities", JSON.stringify(searchedCities))
 
-// saveCityName()
+}
+
+//append the cities in local storage to the ul
+
+var displaySearchedCities = function () {
+ for (i=0; i < searchedCities.length; i++) {
+    var nameEl = document.createElement("li");
+    nameEl.classList = "list-group-item";
+    nameEl.innerHTML(searchedCities[i]);
+    cityList.appendChild(nameEl);
+ }
+
+}
 
 // fetch information from the weather API
 
